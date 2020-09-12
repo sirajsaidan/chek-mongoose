b2e18b940967292bce5ce3d952cde14d5c0308da
sirajsaidan <67007380+sirajsaidan@users.noreply.github.com>
2020-09-12 20:17:36 +0100

    message

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const app= express();
 app.use(bodyParser.urlencoded({
     extended: false
  }));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //read body json//
 
-app.post('/add',(req,res)=>{
+app.post('/add',(req,res)=>{ // create person//
 const person=new Person(req.body)
 person.save();
 res.send("success")
@@ -28,18 +28,50 @@ app.get("/favorit/:food",(req,res)=>{
     })
 
 })
-app.get("/person/:id",(req,res)=>{
-    let findPersonById= function (personId,done){
-        person.findById(personId,(e,r)=>{
-            if(e){
-                console.log(err)
-            }
-            else{
-                done(null,r)
-            }
+app.get("/person/:id",(req,res,next)=>{ //if we dont found with id go to search with Favorite food//
 
-        })
+Person.findById(req.params.id,(e,r)=>{
+    if(!r)
+    next()
+    else{
+        res.json(r)
     }
+    
 })
+})
+app.post("/person/edit",(req,res)=>{
+const {id,newFood}=req.body
+Person.findByIdAndUpdate(id,{$push:{favoriteFoods:newFood}},{new:true},(e,r)=>{
+    res.json(r)
+})    
+})
+app.post("/person/editWithName",(req,res)=>{
+    const {name,age}=req.body
+    Person.findOneAndUpdate({name},{$set:{age}},{new:true},(e,r)=>{
+        res.json(r)
+    })    
+    })
+    app.get("/person/delete/:id",(req,res)=>{
+        Person.findByIdAndRemove(req.params.id,(e,r)=>{
+            res.json(r)
+        })    
+        })
+        app.get("/person/massDelete/:name",async(req,res)=>{
+            console.log(req.params.name)
+         const rest =  await Person.remove({name:req.params.name});
+         res.json(rest)
+            
+          } )    
+          app.get("/person/:food",(req,res)=>{
+              Person.find({favoriteFoods:req.params.food})
+              .sort({name:1})
+              .limit(2)
+              .select({age:0})
+              .exec((e,r)=>{
+                  res.json(r)
+              })
+
+          })
+            
 const PORT=8080
 app.listen(PORT,()=>console.log("App started "))
